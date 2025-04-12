98c57888dfbd31c6db17fd8a4875b415af7539b6
amriteshx1 <amriteshx18@gmail.com>
2025-04-12 22:43:01 +0530

    updated index.js to go as per new database persistence modifications

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const pool = require("../database/pool");
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
-];
+
 
 // GET homepage
-router.get('/', (req, res) => {
-  res.render('index', { title: 'Mini Messageboard', messages });
+router.get('/', async (req, res) => {
+  const result = await pool.query('SELECT * FROM messages ORDER BY added ASC')
+  res.render('index', { title: 'Mini Messageboard', messages: result.rows });
 });
 
 // GET new message form
@@ -25,19 +16,28 @@ router.get('/new', (req, res) => {
 });
 
 // POST new message
-router.post('/new', (req, res) => {
-    const userName = req.body.userName;
-    const userMsg = req.body.userMsg;
-    messages.push({ text: userMsg, user: userName, added: new Date() });
+router.post('/new', async(req, res) => {
+  const { userName, userMsg } = req.body;
+
+  await pool.query(
+    'INSERT INTO messages (text, username) VALUES ($1, $2)',
+    [userMsg, userName]
+  );
     res.redirect("/");
 });
 
 // Open message
-router.get('/message/:id', (req, res) => {
+router.get('/message/:id', async (req, res) => {
   const id = req.params.id;
-  const msg = messages[id];
-  if (!msg) return res.status(404).send("Message not found");
-  res.render('msg', { title: "Mini Messageboard", message: msg });
+  const result = await pool.query('SELECT * FROM messages WHERE id = $1', [id]);
+    const msg = result.rows[0];
+    if (!msg) return res.status(404).send("Message not found");
+
+    console.log(msg);
+    res.render('msg', {
+      title: "Mini Messageboard",
+      message: msg
+    });
 });
 
 module.exports = router;
